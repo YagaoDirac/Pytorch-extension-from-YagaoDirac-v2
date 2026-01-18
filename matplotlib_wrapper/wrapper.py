129fcb6dd0a8a648e31944cf324e565902d1a448
YagaoDirac <yagaodirac@gmail.com>
2026-01-19 00:50:13 +0800

    2026 jan 18

--- matplotlib_wrapper/wrapper.py
+++ matplotlib_wrapper/wrapper.py
@@ -41,19 +41,20 @@ if "marker type defination" and "bc I want to fold it":
             '*','star','h','hexagon1','H','hexagon2',
             '+','plus','x','x','X','x (filled)',
             'D','diamond','d','thin_diamond',
-            '|','vline','_','hline',
-            None]
-    type marker_type = Literal[
+            '|','vline','_','hline']
+    type marker_type = Literal[#do I need this???
             '.',',','o','v','^','<','>',
             '1','2','3','4','8','s','p','P',
-            '*','h','H','+','x','X','D','d','|','_',
-            None]
+            '*','h','H','+','x','X','D','d','|','_']
     pass
-def correct_marker(input:marker_type_raw)->marker_type:
+def correct_marker(input:marker_type_raw|None)->str:#marker_type:
+    if input is None:
+        return ''
     if input in [
             '.',',','o','v','^','<','>',
             '1','2','3','4','8','s','p','P',
             '*','h','H','+','x','X','D','d','|','_']:
+        #assert isinstance(input, str)
         return input
     match input:
         case 'point':              
@@ -115,12 +116,12 @@ def correct_marker(input:marker_type_raw)->marker_type:
     pass#/ function.
 
 if "line type defination" and "bc I want to fold it":
-    type line_type_raw = Literal['-','solid','--','dashed','-.','dash-dot',':','dotted',
-            None]
-    type line_type = Literal['-','--','-.',':',
-            None]
+    type line_type_raw = Literal['-','solid','--','dashed','-.','dash-dot',':','dotted']
+    type line_type = Literal['-','--','-.',':']#do I need this???
     pass
-def correct_line(input:line_type_raw)->line_type:
+def correct_line(input:line_type_raw|None)->str:#line_type:
+    if input is None:
+        return ''
     if input in ['-','--','-.',':']:
         return input
     match input:
@@ -143,14 +144,22 @@ def correct_line(input:line_type_raw)->line_type:
 if "color type defination" and "bc I want to fold it":
     type color_type_raw = Literal['b','blue','g','green','r','red',
                         'c','cyan','m','magenta','y','yellow','k','black',
-                        'w','white',
-            None]
-    type color_type = Literal['b','g','r',
+                        'w','white']
+    type color_type = Literal['b','g','r',#do I need this???
                             'c','m','y','k',
-                            'w',
-            None]
+                            'w']
     pass
-def correct_color(input:color_type_raw)->color_type:
+def correct_color(input:color_type_raw|None|str)->str:#color_type:
+    if input is None:
+        return ''
+    
+    # if input.__len__() == 7:
+    #     assert input[0] == '#'
+    #     for ii in range(1,7):
+    #         assert input[ii] in "012345789abcdefABCDEF"
+    #         pass
+    #     return input
+    
     if input in ['b','g','r',
                 'c','m','y','k',
                 'w']:
@@ -183,13 +192,7 @@ def correct_color(input:color_type_raw)->color_type:
 def torch_Tensor_to_list(tensor:torch.Tensor)->list:
     return tensor.tolist()
 
-def get_current_figure()->figure.Figure:
-    return plt.gcf()
-
-def get_current_axes()->axes.Axes:
-    return plt.gca()
-
-def any_1d_list_to_pylist(input:data_list|None)->list:
+def any_1d_list_to_pylist(input:data_list)->list:
     if "numpy" in sys.modules:
         if isinstance(input, numpy.ndarray):
             return input.tolist()
@@ -198,71 +201,134 @@ def any_1d_list_to_pylist(input:data_list|None)->list:
         if isinstance(input, torch.Tensor):
             return input.tolist()
         pass
-    assert isinstance(input, list) or input is None, f"is this a numpy.ndarray or torch.Tensor? {None\
+    assert isinstance(input, list), f"is this a numpy.ndarray or torch.Tensor? {None\
                             }Umcomment the corresponding import instruct on the top."
     return input
 
-def LineChart_on_axes(axes:axes.Axes = get_current_axes(), data_x:data_list = [], data_y:data_list = [], label = "", \
-                marker:marker_type_raw = None, line:line_type_raw = None, color:color_type_raw = None, \
+
+
+from contextlib import ExitStack
+def wiggle_and_fun(scale: float = 1,length: float = 100,randomness: float = 2)-> ExitStack[bool | None]:
+    return plt.xkcd()
+
+def set_style(index:int)->tuple[str,list[str]]:
+    '''return new_style, all_available_style'''
+    new_style = plt.style.available[index]
+    plt.style.use(new_style)
+    return new_style, plt.style.available
+
+
+
+def get_current_figure()->figure.Figure:
+    return plt.gcf()
+
+def get_current_axes()->axes.Axes:
+    return plt.gca()
+
+
+
+def LineChart_on_axes(data_x:data_list|None = None, data_y:data_list = [], axes:axes.Axes = get_current_axes(), label = "", \
+                marker:marker_type_raw|None = None, line:line_type_raw|None = None, \
+                color:color_type_raw|None = None, color_in_hex_format:str|None = "#000000", \
                         linewidth=2, markersize=12 ,\
                         scalex = True, scaley = True)->list[lines.Line2D]:
-    #all the types:
-    data_x_inner = any_1d_list_to_pylist(data_x)
+    '''The color can be in either format. To use the matplotlib preset color, only set color.
+    To use the hex color format, set color to none, and set color_in_hex_format to any color.
+    '''
+    #marker line color:
+    marker_inner:str = correct_marker(marker)
+    line_inner:str = correct_line(line)
+    if color is None:
+        color_inner:str = ""
+        if color_in_hex_format is None:
+            color_after:str|None = None
+            pass
+        else:
+            color_after = color_in_hex_format
+            pass
+        pass
+    else:#color is not None
+        color_inner = correct_color(color)
+        color_after = None
+        pass
+    fmt = f'{marker_inner}{line_inner}{color_inner}'
+
+    #data
     data_y_inner = any_1d_list_to_pylist(data_y)
     assert data_y_inner.__len__()>0,"At least some data to plot, right?"
-    
-    marker_inner:marker_type|str = correct_marker(marker)
-    line_inner:line_type|str = correct_line(line)
-    color_inner:color_type|str = correct_color(color)
-    
-    fmt = f'{marker_inner}{line_inner}{color_inner}'
-    
-    return axes.plot(data_x_inner, data_y_inner, fmt, label = label, 
-                    linewidth=linewidth, markersize=markersize, 
-                    scalex = scalex, scaley = scaley)
+    if data_x is None:
+        data_x_inner = None
+        result = axes.plot(data_y_inner, fmt, label = label, 
+                        linewidth=linewidth, markersize=markersize, 
+                        scalex = scalex, scaley = scaley)[0]
+        pass#if 
+    else:#data_x is not None
+        data_x_inner = any_1d_list_to_pylist(data_x)
+        assert data_x_inner.__len__() == data_y_inner.__len__()
+        result = axes.plot(data_x_inner, data_y_inner, fmt, label = label, 
+                        linewidth=linewidth, markersize=markersize, 
+                        scalex = scalex, scaley = scaley)[0]
+        pass#else
+    if color_after is not None:
+        result.set_color(color_after)
+        pass
+    return [result]
 
-def set_legend_according_to_artists()->legend.Legend:
+
+
+
+
+def set_legend_according_to_existing_artists()->legend.Legend:
     '''collect all the legend info and add a real legend object into axes.'''
     return plt.legend()
+
+def set_legend(handles:list[artist.Artist], names:list[str]|None = None)->legend.Legend:
+    '''This function only affect the displayed legend. The stored legend in Artists are not modified.
     
-def set_legend(handles:list[artist.Artist]|None = None, 
-                                names:list[str]|None = None)->legend.Legend:
+    The unmentioned Artists will be ignored. So, you can ignore some Artists in legend by not adding them
+    to the handles list.
+    
+    For more parameters:https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.legend.html#matplotlib.pyplot.legend
+    '''
+    for handle in handles:
+        assert isinstance(handle, artist.Artist), "I'm not sure, but this list should probably only contains Artists."
+        pass
+    if names is None:
+        return plt.legend(handles)
+    
+    assert handles.__len__() == names.__len__(), "Check the lists."
     return plt.legend(handles, names)
+
+if "line chart and legend" and __DEBUG_ME__() and False:
+    set_style(5)
     
-    
-if "test" and __DEBUG_ME__() and True:
     data_1_x = torch.tensor([1,2])
     data_1_y = torch.tensor([1,3])
     data_2_x = torch.tensor([5,6])
     data_2_y = torch.tensor([5,7])
 
-    lines_test:list[artist.Artist] = []
-    lines_test.extend(LineChart_on_axes(get_current_axes(), data_1_x, data_1_y,
-                            "data 1111", marker="+", line="--"))
-    LineChart_on_axes(get_current_axes(), data_2_x, data_2_y, 
-                    "data 2222", marker="2", line='-.', markersize=15)
-    set_legend(lines_test,["11111111"])
+    line2d_1 = LineChart_on_axes(data_1_x, data_1_y, get_current_axes(), 
+                            "data 1111", marker="+", line="--")[0]
+    line2d_2 = LineChart_on_axes(data_x=data_2_x, data_y=data_2_y, 
+                    label="data 2222", marker="2", line="-", linewidth=5, markersize=15,
+                    color_in_hex_format="#CA1616")[0]
     
-    #set_legend_according_to_artists()
+    set_legend([line2d_1], ["new label for 1"])
     
     plt.show()
+    #you should see 2 lines and legend with only 1 entry.
     pass
 
 
+def Contour_fill_on_axes():
+    1w 继续。
+    plt.contourf()
 
 
 
-
-
-
-
-
-1w 继续。
-
-
-https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
-fig, ax = plt.subplots()
-im = ax.imshow(harvest)
+# https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+# fig, ax = plt.subplots()
+# im = ax.imshow(harvest)
 
 
 
@@ -277,13 +343,13 @@ vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
 farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
            "Agrifun", "Organiculture", "BioGoods Ltd.", "Cornylee Corp."]
 
-harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
-                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
-                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
-                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
-                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
-                    [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
-                    [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
+harvest = numpy.array( [[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
+                        [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
+                        [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
+                        [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
+                        [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
+                        [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
+                        [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
 
 
 fig, ax = plt.subplots()
@@ -312,7 +378,13 @@ plt.show()
 
 
 
-
+# 还有 hist
+# 还有 hist
+# 还有 hist
+# 还有 hist
+# 还有 hist
+# 还有 hist
+# 还有 hist
 
 
 
