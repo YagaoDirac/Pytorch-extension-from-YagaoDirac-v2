7c4e10657ae5227048d41f685579f04fcdb1bdff
YagaoDirac <yagaodirac@gmail.com>
2026-07-24 01:17:06 +0800

    2026 jul 23

--- DNN2026/basic idea test.py
+++ DNN2026/basic idea test.py
@@ -27,48 +27,30 @@ def _line_():
 
 
 
-# 几个需要单独测试的。
 
-# 1是缓冲区的行为
-# 改层的形状
-
-# 2是可能单独写一个gramo？或者单独的优化器？
-
-# 参数变化强度的保护
-
-
-# 一个整体的class
-
-# 从整体的class里面得到新的输入数据，可能牵扯到新的容器。
-
-# 重新做干堆测试。
-
-
-
-
-
-
-if "core idea" and __DEBUG_ME__() and True:
+if "core idea" and __DEBUG_ME__() and False:
     def ____core_idea____():
         if "example 1" and True:
             out_dim = 2
             in_dim = 3
             batch = 5
-            training_buffer__o_i = torch.tensor([   [0.1, 0.2, 0.3],
-                                                    [0.1, 1.2, 0.3]])
-            assert training_buffer__o_i.shape == torch.Size([out_dim, in_dim])
-            _temp_one_hot__o = training_buffer__o_i.argmax(dim=1)
-            input__b_i = torch.tensor([  [11,  22,  33], 
+            #<  input
+            input___b_i = torch.tensor([  [11,   22,   33], 
                                         [111,  122,  133], 
                                         [211,  222,  233], 
                                         [311,  322,  333], 
                                         [411,  422,  433], ])
-            assert input__b_i.shape == torch.Size([batch, in_dim])
-            
-            output__b_o = input__b_i[:, _temp_one_hot__o]
-            assert output__b_o.shape == torch.Size([batch, out_dim])
-
-            assert _tensor_equal(output__b_o, [ [ 33,  22], 
+            assert input___b_i.shape == torch.Size([batch, in_dim])
+            #<  model param
+            training_buffer___o_i = torch.tensor([   [0.1, 0.2, 0.3],
+                                                    [0.1, 1.2, 0.3]])
+            assert training_buffer___o_i.shape == torch.Size([out_dim, in_dim])
+            #<  calc
+            _temp_one_hot___o = training_buffer___o_i.argmax(dim=1)
+            output___b_o = input___b_i[:, _temp_one_hot___o]
+            assert output___b_o.shape == torch.Size([batch, out_dim])
+            #<  assert
+            assert _tensor_equal(output___b_o, [ [ 33,  22], 
                                                 [133, 122], 
                                                 [233, 222], 
                                                 [333, 322], 
@@ -80,22 +62,24 @@ if "core idea" and __DEBUG_ME__() and True:
             out_dim = 3
             in_dim = 4
             batch = 5
-            training_buffer__o_i = torch.tensor([[0.1, 0.2, 0.3, 0.4],
-                                            [0.1, 0.2, 1.3, 0.4],
-                                            [0.1, 1.2, 0.3, 0.4],])
-            assert training_buffer__o_i.shape == torch.Size([out_dim, in_dim])
-            _temp_one_hot__o = training_buffer__o_i.argmax(dim=1)
-            input__b_i = torch.tensor([ [ 11,   22,   33,   44], 
+            #<  input
+            input___b_i = torch.tensor([ [ 11,   22,   33,   44], 
                                         [111,  122,  133,  144], 
                                         [211,  222,  233,  244], 
                                         [311,  322,  333,  344], 
                                         [411,  422,  433,  444], ])
-            assert input__b_i.shape == torch.Size([batch, in_dim])
-            
-            output__b_o = input__b_i[:, _temp_one_hot__o]
-            assert output__b_o.shape == torch.Size([batch, out_dim])
-
-            assert _tensor_equal(output__b_o, [ [ 44,  33,  22], 
+            assert input___b_i.shape == torch.Size([batch, in_dim])
+            #<  model param
+            training_buffer___o_i = torch.tensor([[0.1, 0.2, 0.3, 0.4],
+                                            [0.1, 0.2, 1.3, 0.4],
+                                            [0.1, 1.2, 0.3, 0.4],])
+            assert training_buffer___o_i.shape == torch.Size([out_dim, in_dim])
+            #<  calc
+            _temp_one_hot___o = training_buffer___o_i.argmax(dim=1)
+            output___b_o = input___b_i[:, _temp_one_hot___o]
+            assert output___b_o.shape == torch.Size([batch, out_dim])
+            #<  assert
+            assert _tensor_equal(output___b_o, [ [ 44,  33,  22], 
                                                 [144, 133, 122],
                                                 [244, 233, 222],
                                                 [344, 333, 322],
@@ -106,75 +90,73 @@ if "core idea" and __DEBUG_ME__() and True:
     ____core_idea____()
     pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if "training process    example.    not the final version." and __DEBUG_ME__() and True:
-    def ____training_process_example____():
-        
+if "how to update output    example.    not the final version." and __DEBUG_ME__() and False:
+    def ____how_to_update_example____():
         if "basic updating    explicityly show the shape." and True:
             out_dim = 2
             in_dim = 3
             batch = 1
-            training_buffer__o_i = torch.tensor([[0.1, 0.2, 0.3],
+            training_buffer___o_i = torch.tensor([[0.1, 0.2, 0.3],
                                             [0.1, 1.2, 0.3]])
-            assert training_buffer__o_i.shape == torch.Size([out_dim, in_dim])
+            assert training_buffer___o_i.shape == torch.Size([out_dim, in_dim])
 
-            training_target__b_o = torch.tensor([[1., -1.],])
-            assert training_target__b_o.shape == torch.Size([ batch, out_dim])
-            training_target__b_o_EXPANDi = training_target__b_o.reshape(shape=[batch, out_dim, 1]).expand(size=[-1, -1, in_dim])
+            training_target___b_o = torch.tensor([[1., -1.],])
+            assert training_target___b_o.shape == torch.Size([ batch, out_dim])
+            training_target__b_o_EXPANDi = training_target___b_o.reshape(shape=[batch, out_dim, 1]).expand(size=[-1, -1, in_dim])
             assert training_target__b_o_EXPANDi.shape == torch.Size([batch, out_dim, in_dim])
 
-            input__b_i = torch.tensor([  [1.,  1.,  1.], ])
-            assert input__b_i.shape == torch.Size([batch, in_dim])
-            input__b_EXPANDo_i = input__b_i.reshape(shape=[batch, 1, in_dim]).expand(size=[-1, out_dim, -1])
-            assert input__b_EXPANDo_i.shape == torch.Size([batch, out_dim, in_dim])
+            input___b_i = torch.tensor([  [1.,  1.,  1.], ])
+            assert input___b_i.shape == torch.Size([batch, in_dim])
+            input___b_EXPANDo_i = input___b_i.reshape(shape=[batch, 1, in_dim]).expand(size=[-1, out_dim, -1])
+            assert input___b_EXPANDo_i.shape == torch.Size([batch, out_dim, in_dim])
 
-            what_to_update__before_sum__b_o_i = training_target__b_o_EXPANDi.mul(input__b_EXPANDo_i)
+            what_to_update__before_sum__b_o_i = training_target__b_o_EXPANDi.mul(input___b_EXPANDo_i)
             assert what_to_update__before_sum__b_o_i.shape == torch.Size([batch, out_dim, in_dim])
 
-            what_to_update__o_i = what_to_update__before_sum__b_o_i.sum(dim=0)
-            assert what_to_update__o_i.shape == torch.Size([out_dim, in_dim])
+            what_to_update___o_i = what_to_update__before_sum__b_o_i.sum(dim=0)
+            assert what_to_update___o_i.shape == torch.Size([out_dim, in_dim])
 
-            training_buffer__o_i += what_to_update__o_i
+            training_buffer___o_i += what_to_update___o_i
             pass#/ test
 
         if "basic updating" and True:
+            batch = 5
             out_dim = 2
             in_dim = 3
-            batch = 5
-            training_buffer__o_i = torch.tensor([   [0.1, 0.2, 0.3],
-                                                    [0.1, 1.2, 0.3]])
-
-            training_target__b_o = torch.tensor([   [1., -1.],
-                                                    [1., -1.],
-                                                    [1., -1.],
-                                                    [1., -1.],
-                                                    [1.,  1.],])
-            training_target__b_o_EXPANDi = training_target__b_o.reshape(shape=[batch, out_dim, 1]).expand(size=[-1, -1, in_dim])
-
-            input__b_i = torch.tensor([ [1.,  1.,  1.],
+            #<  dataset
+            input___b_i = torch.tensor([ [1.,  1.,  1.],
                                         [1.,  1.,  1.],
                                         [1.,  1.,  1.],
                                         [1.,  1.,  1.],
                                         [1.,  1.,  1.],])
-            input__b_EXPANDo_i = input__b_i.reshape(shape=[batch, 1, in_dim]).expand(size=[-1, out_dim, -1])
+            label___b_o = torch.tensor([ [1.,  1.],
+                                        [1.,  1.],
+                                        [1.,  1.],
+                                        [1.,  1.],
+                                        [1.,  1.],])
+            #<  optimizer param
+            lr = 0.2
 
-            what_to_update__before_sum__b_o_i = training_target__b_o_EXPANDi.mul(input__b_EXPANDo_i)
-            what_to_update__o_i = what_to_update__before_sum__b_o_i.sum(dim=0)
+            #<  model param
+            training_buffer___o_i = torch.tensor([   [0.1, 0.2, 0.3],
+                                                    [0.1, 1.2, 0.3]])
+            #training_target___b_o____or_empty = torch.empty(size=[])  not for now.
 
-            training_buffer__o_i += what_to_update__o_i
+            #<  forward path
+            the_input_for_this_layer___b_i = input___b_i.detach().clone()
+            #this layer doesn't need to know what it output ed.
+
+            #<  backward path
+            training_target___b_o = label___b_o.detach().clone()
+
+            #<  update the training buffer
+            training_target___b_o_EXPANDi = training_target___b_o.reshape(shape=[batch, out_dim, 1]).expand(size=[-1, -1, in_dim])
+            input___b_EXPANDo_i = the_input_for_this_layer___b_i.reshape(shape=[batch, 1, in_dim]).expand(size=[-1, out_dim, -1])
+
+            what_to_update__before_sum___b_o_i = training_target___b_o_EXPANDi.mul(input___b_EXPANDo_i)
+            what_to_update___o_i = what_to_update__before_sum___b_o_i.sum(dim=0)
+
+            training_buffer___o_i += what_to_update___o_i * lr
             pass#/ test
 
 
@@ -183,13 +165,204 @@ if "training process    example.    not the final version." and __DEBUG_ME__() a
 
 
         return 
-    ____training_process_example____()
+    ____how_to_update_example____()
     pass
 
 
+class DNN_input_container_2026(torch.nn.Module):
+    data:torch.nn.parameter.Parameter
+    size:int
+    _init_to_nan:bool
+    def __init__(self, batch:int, 
+                dtype:torch.dtype|None = None, device:torch.device|str|None = "cpu", 
+                init_capacity = 16, init_to_nan = True):
+        super().__init__()
+        self.data = torch.nn.Parameter(torch.empty(size=[batch, init_capacity], 
+                    dtype=dtype, device=device))
+        self.data.requires_grad_(False)
+        self.size = 0
+        self._init_to_nan = init_to_nan
+        if init_to_nan:
+            self.data.fill_(torch.nan)
+            pass
+        pass
+    def batch(self)->int:
+        '''get'''
+        return self.data.shape[0]
+    def capacity(self)->int:
+        '''get'''
+        return self.data.shape[1]
+    def size(self)->int:
+        '''get'''
+        return self.size
+    def squeeze(self):
+        assert False, "unfinished"
+
+    def __calc_bigger_capacity(self)->int:
+        return int(self.capacity()*2)
+
+    def extend(self, other:torch.Tensor)->None:
+        assert other.shape.__len__() == 2
+        assert other.shape[0] == self.data.shape[0]
+        with torch.no_grad():
+                
+            _temp__how_many_to_add = other.shape[1]
+            _size_after = self.size + _temp__how_many_to_add
+            if _size_after > self.capacity():# get a bigger new capacity first.
+                _temp___new_capacity = self.__calc_bigger_capacity()
+                _temp___new_container = torch.empty(size=[self.batch(), _temp___new_capacity])
+                if self._init_to_nan:
+                    _temp___new_container.fill_(torch.nan)
+                    pass
+                _temp___new_container[:, 0:self.size] = self.get_useful()
+                self.data.data = _temp___new_container
+                pass
+
+            self.data[:, self.size:self.size + _temp__how_many_to_add] = other
+            self.size = _size_after
+            return
+        pass#end of function
+
+    def get_useful(self)->torch.Tensor:
+        result = self.data[:,:self.size]
+        return result
+
+    pass
+
+if "how to add input." and __DEBUG_ME__() and True:
+    def ____input_container_idea____():
+        if "basic idea" and True:
+            batch = 2
+            #<  the container
+            the_container = DNN_input_container_2026(batch=batch,init_capacity=6, )
+            assert the_container.size == 0
+            assert the_container.capacity() == 6
+            assert the_container._init_to_nan == True
+            the_container.extend(torch.tensor([ [ 11,  22,  33],
+                                                [111, 122, 133],]))
+            assert the_container.size == 3
+            assert the_container.capacity() == 6
+            the_container.extend(torch.tensor([ [ 77,  88],
+                                                [177, 188],]))
+            assert the_container.size == 5
+            assert the_container.capacity() == 6
+            assert _tensor_equal(the_container.get_useful(), torch.tensor([ 
+                                                [ 11,  22,  33,  77,  88],
+                                                [111, 122, 133, 177, 188],]))
+            assert torch.isnan(the_container.data[:,5]).all()
+
+            the_container.extend(torch.tensor([ [ 111,  222],
+                                                [1111, 1222],]))
+            assert the_container.size == 7
+            assert the_container.capacity() == 12
+            assert _tensor_equal(the_container.get_useful(), torch.tensor([ 
+                                                [ 11,  22,  33,  77,  88,  111,  222],
+                                                [111, 122, 133, 177, 188, 1111, 1222],]))
+            assert torch.isnan(the_container.data[:,7:]).all()
+
+            pass
+
+        if "device adaption" and True:
+            the_container = DNN_input_container_2026(batch=2,init_capacity=6 )
+            assert the_container.data.device.type == "cpu"
+            the_container.cuda()
+            assert the_container.data.device.type == "cuda"
+            the_container = DNN_input_container_2026(batch=2,init_capacity=6, device="cuda")
+            assert the_container.data.device.type == "cuda"
+            the_container.cpu()
+            assert the_container.data.device.type == "cpu"
+            pass
+
+
+        return
+    ____input_container_idea____()
 
 
 
+# 改层的形状，删结果和加输入
+# 1是缓冲区的行为，更新当中的更新动力学？？？可能单独写一个gramo？或者单独的优化器？
+# 一个整体的容器
+#trace back 需要容器的支持。 从整体的class里面得到新的输入数据。
+# 重新做干堆测试。
+
+if "delete output" and __DEBUG_ME__() and True:
+    def ____delete_output____():
+        if "delete output" and True:
+
+            batch = 2
+            out_dim = 5
+            in_dim = 3
+            #<  the answer
+            keep_these_output = torch.tensor([1,1,1,0,1], dtype=torch.bool)
+            new_out_dim = int(keep_these_output.sum().item())
+            #assert isinstance(new_out_dim, int)
+
+            #<  dataset
+            input___b_i = torch.tensor([[1.,  1.,  1.],
+                                        [1.,  1.,  1.],])
+            label___b_o = torch.tensor([[1.,  1.,  1.,  1.,  1.],
+                                        [1.,  1.,  1.,  1.,  1.],])
+            #<  model param
+            ori___training_buffer___o_i = torch.tensor([  
+                                                    [0.1, 0.2, 0.3],
+                                                    [0.1, 1.2, 0.3],
+                                                    [0.1, 0.2, 0.3],
+                                                    [0.1, 1.2, 0.3],
+                                                    [1.1, 0.2, 0.3],])#32321
+            #<  original    forward path
+            _temp_one_hot___o = ori___training_buffer___o_i.argmax(dim=1)
+            ori___output___b_o = input___b_i[:, _temp_one_hot___o]
+            del _temp_one_hot___o
+            assert ori___output___b_o.shape == torch.Size([batch, out_dim])
+            #<  the new shape
+            new___training_buffer___o_i = ori___training_buffer___o_i[keep_these_output,:]
+            assert new___training_buffer___o_i.shape == torch.Size([new_out_dim, in_dim])
+            _temp___new____one_hot___o = new___training_buffer___o_i.argmax(dim=1)
+            new___output___b_o = input___b_i[:, _temp___new____one_hot___o]
+            del _temp___new____one_hot___o
+            #<  assert 
+            assert _tensor_equal(new___output___b_o, ori___output___b_o[:, keep_these_output])
+
+            pass#/ test
+
+        if "delete output" and True:
+            for batch in [2,5,10]:
+                for out_dim in [3,7,11]:
+                    for in_dim in [6,9,13]:
+                        for _ in range(5):
+                            #<  the answer
+                            keep_these_output = torch.rand(size=[out_dim])
+                            keep_these_output = keep_these_output.gt(0.5)
+
+                            new_out_dim = int(keep_these_output.sum().item())
+                            #assert isinstance(new_out_dim, int)
+
+                            #<  dataset
+                            input___b_i = torch.randn(size=[batch, in_dim])
+                            label___b_o = torch.randn(size=[batch, out_dim])
+                            #<  model param
+                            ori___training_buffer___o_i = torch.randn(size=[out_dim, in_dim])
+                            #<  original    forward path
+                            _temp_one_hot___o = ori___training_buffer___o_i.argmax(dim=1)
+                            ori___output___b_o = input___b_i[:, _temp_one_hot___o]
+                            del _temp_one_hot___o
+                            assert ori___output___b_o.shape == torch.Size([batch, out_dim])
+                            #<  the new shape
+                            new___training_buffer___o_i = ori___training_buffer___o_i[keep_these_output,:]
+                            assert new___training_buffer___o_i.shape == torch.Size([new_out_dim, in_dim])
+                            _temp___new____one_hot___o = new___training_buffer___o_i.argmax(dim=1)
+                            new___output___b_o = input___b_i[:, _temp___new____one_hot___o]
+                            del _temp___new____one_hot___o
+                            #<  assert 
+                            assert _tensor_equal(new___output___b_o, ori___output___b_o[:, keep_these_output])
+                            pass#for _
+                        pass#for in_dim
+                    pass#for out_dim
+                pass#for batch
+            pass#/ test
+
+        return 
+    ____delete_output____()
 
 
 
