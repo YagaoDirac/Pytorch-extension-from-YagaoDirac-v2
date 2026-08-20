cd4b4648be48d747b01ca889237484585ef38dc6
YagaoDirac <yagaodirac@gmail.com>
2026-08-21 03:09:45 +0800

    2026 aug 20 a

--- tutorial_for_beginners/a reshape bug.py
+++ tutorial_for_beginners/a reshape bug.py
@@ -1,58 +1,87 @@
 import torch
 
-# class bug_report_model(torch.nn.Module):
-#     def __init__(self, param:torch.Tensor|None = None, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         if param is None:
-#             self.param = torch.nn.Parameter(torch.randn(size=[3,2]))
-#             pass
-#         else:
-#             self.param = torch.nn.Parameter(param)
-#             pass
+'''
+What is this test.
 
-#         assert self.param.requires_grad == True
-#         return
-#     def forward(self, input:torch.Tensor)->torch.Tensor:
-#         output = input@(self.param.T)
-#         return output
+In pytorch 2.12, there exists a feature. When you create a torch.nn.Parameter, the 
+creation behavior stores some shape related info in some backend. Idk the detail, but 
+probably some backend. If you set the data inside the Parameter object, that stored
+info is not updated. So if you use this Parameter later, pytorch raises with some
+shape error.
 
-#     def break_the_shape(self)->None:
-#         self.param.data = torch.randn(size=[5,2])
-#         return
-#     #end of class
+In order to counter this, the simpliest method is to store the Parameter inside 
+a torch.nn.ParameterList. Then, every time you need auto completion, you write
+torch.nn.Parameter.the_name and then ctrl+space(the short cut in vs code.). 
+When you need to set the data inside the Parameter, you do
+self.the_param_list[0] = torch.nn.Parameter(torch.tensor([1., 2, 3], dtype = torch.float32))
+Keep it in mind, in pytorch, only float point number is allowed to require_grad.
 
-# assert torch.__version__ == '2.12.1+cu132'
+In the part_1, I showed how to refresh the entire torch.nn.Module subclass object to 
+solve this problem. In part_2, I showed how to use torch.nn.ParameterList to solve it.
 
-# batch = 7
-# #infra
-# the_ori_model = bug_report_model()
-# #epoch 0
-# output___batch_3:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
-# assert output___batch_3.shape == torch.Size([batch, 3])
-# output___batch_3.backward(gradient=torch.randn_like(output___batch_3))
-# del output___batch_3
-# #I don't need the grad. No optim.step()
-# the_ori_model.param.grad = None#zero_grad
-# #a completely reshape of the inner data.
-# the_ori_model.break_the_shape()
+This issue is from some assumption. Similar assumptions are usually made bc the 
+way the tool is design. Although I call it a bug, but to be fair, it's not a bug.
+I don't believe a lot of you guys need to reshape the data inside a parameter the way
+I show in this test. So, if you don't reshape it like this, you don't need this trick.
+'''
 
-# #epoch 1
-# output___batch_5:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
-# assert output___batch_5.shape == torch.Size([batch, 5])
-# #output___batch_5.backward(gradient=torch.randn_like(output___batch_5)) this line doesn't work. 
-# del output___batch_5
+def part_1():
+    class bug_report_model(torch.nn.Module):
+        def __init__(self, param:torch.Tensor|None = None, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if param is None:
+                self.param = torch.nn.Parameter(torch.randn(size=[3,2]))
+                pass
+            else:
+                self.param = torch.nn.Parameter(param)
+                pass
 
-# the_new_model = bug_report_model(the_ori_model.param.data)
-# del the_ori_model
+            assert self.param.requires_grad == True
+            return
+        def forward(self, input:torch.Tensor)->torch.Tensor:
+            output = input@(self.param.T)
+            return output
 
-# #epoch 1 but with the new object
-# new___output___batch_5:torch.Tensor = the_new_model(torch.randn(size=[batch, 2]))
-# assert new___output___batch_5.shape == torch.Size([batch, 5])
-# assert the_new_model.param.grad is None
-# new___output___batch_5.backward(gradient=torch.randn_like(new___output___batch_5))# this line works.
-# assert the_new_model.param.grad is not None
+        def break_the_shape(self)->None:
+            self.param.data = torch.randn(size=[5,2])
+            return
+        #end of class
 
-# print("new model object works.")
+    assert torch.__version__ == '2.12.1+cu132'
+
+    batch = 7
+    #infra
+    the_ori_model = bug_report_model()
+    #epoch 0
+    output___batch_3:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
+    assert output___batch_3.shape == torch.Size([batch, 3])
+    output___batch_3.backward(gradient=torch.randn_like(output___batch_3))
+    del output___batch_3
+    #I don't need the grad. No optim.step()
+    the_ori_model.param.grad = None#zero_grad
+    #a completely reshape of the inner data.
+    the_ori_model.break_the_shape()
+
+    #epoch 1
+    output___batch_5:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
+    assert output___batch_5.shape == torch.Size([batch, 5])
+    #output___batch_5.backward(gradient=torch.randn_like(output___batch_5)) this line doesn't work. 
+    del output___batch_5
+
+    the_new_model = bug_report_model(the_ori_model.param.data)
+    del the_ori_model
+
+    #epoch 1 but with the new object
+    new___output___batch_5:torch.Tensor = the_new_model(torch.randn(size=[batch, 2]))
+    assert new___output___batch_5.shape == torch.Size([batch, 5])
+    assert the_new_model.param.grad is None
+    new___output___batch_5.backward(gradient=torch.randn_like(new___output___batch_5))# this line works.
+    assert the_new_model.param.grad is not None
+
+    print("new model object works.")
+
+    return
+part_1()
 
 
 
@@ -66,43 +95,45 @@ import torch
 
 
 '''Ok now we have a part 2. Enjoy'''
-class can_I_replace_the_element_inside_a_param_list__without_refresh_the_entire_object(torch.nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+def part_2():
+    class can_I_replace_the_element_inside_a_param_list__without_refresh_the_entire_object(torch.nn.Module):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
-        self.param_list = torch.nn.ParameterList([torch.nn.Parameter(torch.rand(size=[3,2]))])
-        assert self.param_list[0].requires_grad == True
+            self.param_list = torch.nn.ParameterList([torch.nn.Parameter(torch.rand(size=[3,2]))])
+            assert self.param_list[0].requires_grad == True
 
-        return
-    def forward(self, input:torch.Tensor)->torch.Tensor:
-        output = input@(self.param_list[0].T)
-        return output
+            return
+        def forward(self, input:torch.Tensor)->torch.Tensor:
+            output = input@(self.param_list[0].T)
+            return output
 
-    def break_the_shape(self)->None:
-        self.param_list[0] = torch.nn.Parameter(torch.rand(size=[5,2]))
-        return
-    #end of class
+        def break_the_shape(self)->None:
+            self.param_list[0] = torch.nn.Parameter(torch.rand(size=[5,2]))
+            return
+        #end of class
 
 
-batch = 7
-#infra
-the_ori_model = can_I_replace_the_element_inside_a_param_list__without_refresh_the_entire_object()
-#epoch 0
-output___batch_3:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
-assert output___batch_3.shape == torch.Size([batch, 3])
-output___batch_3.backward(gradient=torch.randn_like(output___batch_3))
-del output___batch_3
-#I don't need the grad. No optim.step()
-the_ori_model.param_list[0].grad = None#zero_grad
-#a completely reshape of the inner data.
-the_ori_model.break_the_shape()
+    batch = 7
+    #infra
+    the_ori_model = can_I_replace_the_element_inside_a_param_list__without_refresh_the_entire_object()
+    #epoch 0
+    output___batch_3:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
+    assert output___batch_3.shape == torch.Size([batch, 3])
+    output___batch_3.backward(gradient=torch.randn_like(output___batch_3))
+    del output___batch_3
+    #I don't need the grad. No optim.step()
+    the_ori_model.param_list[0].grad = None#zero_grad
+    #a completely reshape of the inner data.
+    the_ori_model.break_the_shape()
 
-#epoch 1
-output___batch_5:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
-assert output___batch_5.shape == torch.Size([batch, 5])
-output___batch_5.backward(gradient=torch.randn_like(output___batch_5))# this line works. 
-print("The elements of a param_list can be replaced with totally different shape and still works.")
-
+    #epoch 1
+    output___batch_5:torch.Tensor = the_ori_model(torch.randn(size=[batch, 2]))
+    assert output___batch_5.shape == torch.Size([batch, 5])
+    output___batch_5.backward(gradient=torch.randn_like(output___batch_5))# this line works. 
+    print("The elements of a param_list can be replaced with totally different shape and still works.")
+    return
+part_2()
 
 
 
